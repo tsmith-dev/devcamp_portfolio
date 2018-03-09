e215e1c6e79f3fe03aded5a2092750aa6d2fa9f8
Travis <mr_travis_smith@hotmail.com>
2018-03-09 00:19:10 -0600

    add portfolio edit functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,10 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
     if @portfolio_item.save
@@ -16,6 +20,17 @@ class PortfoliosController < ApplicationController
       render new
     end
   end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    if @portfolio_item.update(portfolio_params)
+      redirect_to portfolios_path
+    else
+      render edit
+    end
+  end
+
+  private
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
