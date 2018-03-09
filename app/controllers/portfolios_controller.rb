cd3e67363c527f3b0c43c3c49b7af2af8e056931
Travis <mr_travis_smith@hotmail.com>
2018-03-09 00:00:53 -0600

    add portfolio form and new/create actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,20 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    if @portfolio_item.save
+      redirect_to portfolios_path
+    else
+      render new
+    end
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
 end
